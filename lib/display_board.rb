e1dc1a3a29b9dc20352d83ea9897a35a68f77ef5
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-03-20 23:00:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,3 +2,10 @@
 def display_board
   puts "A Tic Tac Toe Board"
 end
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
